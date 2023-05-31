5026e2668552f87a5cbc91513ce25d5afc37d439
sarawut <sarawutkai99@gmail.com>
2023-05-31 11:55:04 +0700

    navbar

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,20 @@ export default function index() {
     <Head>
       <title>Welcome to NextJs</title>
     </Head>
+    <nav className="navbar bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
+  <div className="container-fluid">
+  <a className="navbar-brand" href="#">
+  <img src="/xcxc.jpg" alt="" width="30" height="24" className="d-inline-block align-text-top" />
+  charlotte
+</a>
+    
+    <form className="d-flex" role="search">
+      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+      <button className="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
     <div className="text-center">Welcome</div>
     <br />
     <div className="text-center"><button type="button" className="btn btn-outline-success">index</button></div>
