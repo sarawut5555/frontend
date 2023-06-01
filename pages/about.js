40e20cdfce8a01a55409225119ed835fd9f1a93f
sarawut <sarawutkai99@gmail.com>
2023-06-01 09:16:43 +0700

    update about

---
+++ pages/about.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function about() {
+  return (
+    <>
+    <Head>
+      <title>About</title>
+    </Head>
+    <header data-bs-theme="dark">
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div className="container-fluid">
+        <Link className="navbar-brand" href="/">
+        <Image src="/xcxc.jpg" alt="Logo" width={30} height={24} className="bd-placeholder-img rounded-circle" />
+            charlotte
+        </Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+        <ul className="navbar-nav me-auto mb-2 mb-md-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" href="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" href="/about">about</Link>
+            </li>
+            <li className="nav-item dropdown">
+                <Link className="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Contact
+                </Link>
+                <ul className="dropdown-menu">
+                    <li><Link className="dropdown-item" href="#">Line</Link></li>
+                    <li><Link className="dropdown-item" href="#">Instagram</Link></li>
+                    <li><Link className="dropdown-item" href="#">facebook</Link></li>
+                </ul>
+            </li>
+          </ul>
+        <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+    </header>
+
+    <main>
+        <br />
+        <br />
+        <br />
+    <hr className="featurette-divider" />    
+    <div className="row featurette">
+        <div className="col-md-7 order-md-2">
+          <h2 className="featurette-heading fw-normal lh-1">Oh yeah, it’s that good. <span className="text-body-secondary">See for yourself.</span></h2>
+          <p className="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+        </div>
+        <div className="col-md-5 order-md-1">
+            <Image className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width={500} height={500} src="/xcxc.jpg"></Image>
+        </div>
+      </div>
+    </main>
+
+
+
+    </>
+  )
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function index() {
   return (
@@ -12,20 +13,30 @@ export default function index() {
   <header data-bs-theme="dark">
     <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Carousel</a>
+      <Link className="navbar-brand" href="/">
+        <Image src="/xcxc.jpg" alt="Logo" width={30} height={24} className="bd-placeholder-img rounded-circle" />
+            charlotte
+        </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <ul className="navbar-nav me-auto mb-2 mb-md-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <Link className="nav-link active" aria-current="page" href="/">Home</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
+              <Link className="nav-link active" href="/about">about</Link>
             </li>
-            <li className="nav-item">
-              <a className="nav-link disabled">Disabled</a>
+            <li className="nav-item dropdown">
+                <Link className="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Contact
+                </Link>
+                <ul className="dropdown-menu">
+                    <li><Link className="dropdown-item" href="#">Line</Link></li>
+                    <li><Link className="dropdown-item" href="#">Instagram</Link></li>
+                    <li><Link className="dropdown-item" href="#">Facebook</Link></li>
+                </ul>
             </li>
           </ul>
           <form className="d-flex" role="search">
@@ -36,6 +47,7 @@ export default function index() {
       </div>
     </nav>
   </header>
+  
   <main>
     <div id="myCarousel" className="carousel slide mb-6" data-bs-ride="carousel" data-bs-theme="light">
       <div className="carousel-indicators">
